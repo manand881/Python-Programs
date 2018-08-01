97bbdcdb2142cdfbeba70033618b5fedb1bbc89a
Anand <manand881@gmail.com>
2018-08-01 14:26:24 +0530

    commit 3

---
+++ print calendar.py
@@ -0,0 +1,19 @@
+# import calendar
+# year=int(input("enter the year:"))
+# monthm=int(input("enter the month:"))
+# print(calendar.month(year,monthm))
+# import calendar
+# y = int(input("Input the year : "))
+# m = int(input("Input the month : "))
+# print(calendar.month(y, m))
+# import calendar
+
+# year = int(raw_input("the year: "))
+# month = int(raw_input("the month: "))
+
+# # txt_cal = calendar.TextCalendar()
+# # txt_cal.prmonth(year, month)
+
+# calendar.prmonth(year, month)
+import calendar
+print(calendar.month(int(input("Enter Year: ")),int(input("Enter Month: "))))
